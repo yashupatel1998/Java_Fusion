e0f463c9463382e33e589410cd19e0550e342afa
PritBPatel1845 <pritpatel1845@gmail.com>
2024-12-24 03:03:37 -0500

    init commit

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,2 +1,39 @@
 public class QuickSort {
+    private static int partition(int[] arry, int low, int high) {
+        int pivot = arry[high];
+        int i = low - 1;
+        for (int j = 0; j < high; j++) {
+            if (arry[j] < pivot) {
+                i++;
+                int temp = arry[i];
+                arry[i] = arry[j];
+                arry[j] = temp;
+            }
+
+        }
+        i++;
+        int temp = arry[i];
+        arry[i] = arry[high];
+        arry[high] = temp;
+        return i;
+    }
+
+    private static void quickShort(int[] arry, int low, int high) {
+        if(low<high){
+            int pi = partition(arry, low, high);
+            quickShort(arry, low, pi-1);
+            quickShort(arry, pi+1, high);
+        }
+    }
+    public static void main(String[] args){
+        int [] arry = {6,3,2,5,8,9};
+        int n = arry.length;
+        quickShort(arry, 0, n-1);
+
+        for (int j : arry) {
+            System.out.println(j + " ");
+        }
+        System.out.println();
+    }
+
 }
