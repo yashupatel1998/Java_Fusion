8d96614e860dba342bfc1f9bdb7c6e324f9901d7
yashupatel1998 <yashasvipatel19@gmail.com>
2025-01-01 07:18:28 -0500

    init commit

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -25,11 +25,21 @@ public class BinaryTree {
             return newNode;
         }
 
+        // PreorderTraversals
+        public static void preOrder(Node root){
+            if(root == null){
+                return;
+            }
+            System.out.print(root.data + " ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+
         public static void main(String[] args) {
             int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
             BinaryTreeIS tree = new BinaryTreeIS();
             Node root = tree.binaryTreeInsertion(nodes);
-            System.out.println(root.data);
+            preOrder(root);
         }
     }
 }
